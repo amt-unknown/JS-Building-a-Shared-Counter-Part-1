8af2209eb56a44400def368174d1b098276d0d97
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-02-16 21:52:07 -0800

    Implemented server updating and cross user page synchronization

--- public/index.js
+++ public/index.js
@@ -4,23 +4,43 @@ async function main(){
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let response = await fetch("http://localhost:9001/counter")
-    let jsonData = await response.json()
+    let response = await fetch("http://localhost:9001/counter");
+    let jsonData = await response.json();
 
-    let countValue = jsonData.value;
+    let initCountValue = jsonData.value
+    let countValue = initCountValue;
+
+    setInterval(async ()=>{
+        let sync_response = await fetch("http://localhost:9001/counter");
+        countValue = (await sync_response.json()).value;
+        countContainer.textContent = countValue;
+    }, 200) //Set to 100-500ms
+
 
     function refresh(){
-        countContainer.textContent = jsonData.value
+        countValue = initCountValue;
+        countContainer.textContent = countValue;
+        update()
+    }
+
+    async function update(){
+        await fetch("http://localhost:9001/counter", {
+            method:"PATCH",
+            headers: {'Content-Type': "application/json"},
+            body: JSON.stringify({'value': String(countValue)})
+        });
     }
 
     function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        update();
     }
 
     function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        update();
     }
 
     refreshButton.addEventListener('click', refresh)
